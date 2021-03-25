263e0576ce43d26f521d3018b0d63000207ce727
loumioussama28 <47556294+loumioussama28@users.noreply.github.com>
2021-03-25 09:34:02 +0100

    fix ResultQCM

--- routes/resultApi.js
+++ routes/resultApi.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const Result = require('../models/resultSchema');
 const passport = require('passport');
+const Question = require('../models/questionSchema');
+
 
 const router = express.Router();
 
@@ -16,6 +18,24 @@ router.post('/results',  passport.authenticate("bearer", { session: false }),asy
     res.json(createdResult);
 });
 
+////ResultTest
+router.get('/results/:idTest', async (req, res) => {
+    const resultQCM = await Result.findById(req.params.idTest);
+    res.json(resultQCM);
+});
+
+
+////VerificationduTestByQuestion
+router.get('/results/:idTest/:idQCM/:idQuestion', async (req, res) => {
+    const resultQCM = await Result.findById(req.params.idTest)
+    .map(x => x.answers.find(x => x.idQuestion==req.params.idQuestion));
+    const answerQCM = await Question.findById(req.params.idQCM)
+    .map(x => x.description.find(x => x._id==req.params.idQuestion));
+    console.log(answerQCM);
+    let Test=false;
+    (answerQCM.answer==resultQCM.answer) ? Test=true : Test;
+    res.json(Test);
+});
 
 
 
